5c4fc6eb766fc282e20e2521ca31b8319f3bdadf
AMilburn <AMilburn@users.noreply.github.com>
2026-03-12 11:47:43 +0100

    Resolve Tailwind dependency for Turbopack

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // @ts-ignore
+  turbopack: {
+    resolveAlias: {
+      tailwindcss: "./node_modules/tailwindcss",
+    },
+  },
 };
 
 export default nextConfig;
